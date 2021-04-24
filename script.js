a211e6b158c84f2d634a440c5d39ca89511353e5
v341196137 <38384400+vivian-dai@users.noreply.github.com>
2021-04-24 01:19:48 -0400

    documentation
    images from co-author
    
    Co-Authored-By: CLbox <51213177+cleibox@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+/**
+ * @fileoverview client side JS
+ */
+
+// create the socket
 var socket = io.connect();
--- server.js
+++ server.js
@@ -1,18 +1,35 @@
+/**
+ * @fileoverview server for YRHacks simulation
+ */
+
+// constants
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require("socket.io")(server);
 
+// send all file things over to client
 app.use(express.static(__dirname));
+/**
+ * send index.html to thc client as the file to serve
+ */
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+/**
+ * starts server listen
+ * @param port 3000 the port to listen to
+ */
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
 
+/**
+ * waits for socket connections
+ * @param socket the socket to connect to
+ */
 io.sockets.on('connection', function(socket) {
   console.log("new connection");
 });
