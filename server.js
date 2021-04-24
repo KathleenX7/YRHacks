8325a2c3fef12db1662bc625fef0baa79121f757
v341196137 <38384400+vivian-dai@users.noreply.github.com>
2021-04-23 20:59:40 -0400

    serves index.html

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const io = require("socket.io")(server);
 
+app.use(express.static(__dirname));
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
+});
+
+io.sockets.on('connection', function(socket) {
+  console.log("new connection");
 });
